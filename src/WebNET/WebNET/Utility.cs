02c0fe71fc30ed7832afbdbb74c8aabad952548f
ZitaRR <Haeggqvist1998@hotmail.com>
2021-06-07 03:20:25 +0200

    Client handshake
    Implemented handshake between server and client

--- src/WebNET/WebNET/Utility.cs
+++ src/WebNET/WebNET/Utility.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebNET
 {
     internal static class Utility
     {
+        private const string EOL = "\r\n";
+        private const string GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+
+        private static readonly Regex getRegex = new("^GET");
+        private static readonly Regex keyRegex = new("Sec-WebSocket-Key: (.*)");
+
         /// <summary>
         ///     Attempts to handshake the client
         /// </summary>
@@ -16,7 +24,30 @@ namespace WebNET
         /// <returns>Bool</returns>
         internal static async Task<bool> TryHandshakeAsync(TcpClient tcp)
         {
-            throw new NotImplementedException(nameof(TryHandshakeAsync));
+            Socket socket = tcp.Client;
+            NetworkStream stream = tcp.GetStream();
+
+            byte[] buffer = new byte[556];
+            await socket.ReceiveAsync(buffer, SocketFlags.None);
+            string headers = Encoding.UTF8.GetString(buffer);
+
+            bool isGet = getRegex.IsMatch(headers);
+            if (isGet)
+            {
+                string key = keyRegex.Match(headers).Groups[1].Value.Trim() + GUID;
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                string sha1 = Convert.ToBase64String(SHA1.Create().ComputeHash(keyBytes));
+                byte[] response = Encoding.UTF8.GetBytes(
+                    $"HTTP/1.1 101 Switching Protocols{EOL}" +
+                    $"Connection: Upgrade{EOL}" +
+                    $"Upgrade: websocket{EOL}" +
+                    $"Sec-WebSocket-Accept: {sha1}{EOL}{EOL}");
+                await stream.WriteAsync(response.AsMemory(0, response.Length));
+                await stream.FlushAsync();
+                return true;
+            }
+
+            return false;
         }
 
         /// <summary>
